6504c47fd5047f9dc49ce89b59b46f3db50bef80
hotsnow199 <49254639+hotsnow199@users.noreply.github.com>
2021-03-11 00:00:09 +0700

    add carousel component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,12 @@
 import { Component } from 'react';
 import './pagestyle/home.css';
+import Carousel from '../libs/utils/Carousel';
 export default class Home extends Component {
   render() {
-    return <div className='home-wrapper'></div>;
+    return (
+      <div className='home-wrapper'>
+        <Carousel auto={true} speed={2000} />
+      </div>
+    );
   }
 }
