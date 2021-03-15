17749a3211af41f61a83c379c934d376fc424515
valbenia <huynguyen.rido@gmail.com>
2021-03-15 22:54:55 +0700

    test footer

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -6,10 +6,12 @@ import { connect } from 'react-redux';
 import Home from './components/pages/Home';
 import { setRouteAction } from './redux/actionCreators/routeAction';
 import Header from './components/libs/Header/Header';
-import ErrorPage from './components/pages/ErrorPage';
+import Footer from './components/libs/Footer/Footter';
+
 function AppRouter({ dispatch }) {
   let location = useLocation();
   useEffect(() => {
+    console.log(setRouteAction());
     dispatch(setRouteAction(location.pathname));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location.pathname]);
@@ -17,22 +19,18 @@ function AppRouter({ dispatch }) {
   return (
     <>
       <Header />
-      <main>
-        <Switch>
-          <Route path='/' exact>
-            <Home />
-          </Route>
-          <Route path='/about'>
-            <h1>render about route</h1>
-          </Route>
-          <Route path='*'>
-            <ErrorPage
-              errorMessage='PAGE NOT FOUND 404!'
-              subMessage='Sorry we couldn’t find your page'
-            />
-          </Route>
-        </Switch>
-      </main>
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <h1>render about route</h1>
+        </Route>
+        <Route path='*'>
+          <h1>render error route</h1>
+        </Route>
+      </Switch>
+      <Footer />
     </>
   );
 }
--- src/components/libs/Footer/Footter.js
+++ src/components/libs/Footer/Footter.js
@@ -1,4 +1,49 @@
-const Footer = () => {
-  return <footer>{/* Huy code here ! */}</footer>;
-};
-export default Footer;
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { connect } from "react-redux";
+import smallLogo from "../../../img/small-logo.png";
+import "./libstyle/footer.css";
+import { Container } from "@material-ui/core";
+function Footer({ dispacth }) {
+  const matchesMedia = useMediaQuery("(min-width:1000px)");
+
+  return (
+    <footer>
+      {matchesMedia ? null : 1}
+      <Container>
+        <section className={"footer-top-content"}>
+          <div className="footer-top-left">
+            <div className="footer-logo">
+              <img src={smallLogo} />
+            </div>
+            <div className="footer-info">
+              <p>Search</p>
+              <p>Contact Us</p>
+              <p>Shipping</p>
+              <p>Returns + Exchanges</p>
+              <p>Terms + Conditions</p>
+              <p>Privacy policy</p>
+              <p>Our story</p>
+            </div>
+          </div>
+          <div className="footer-top-right">
+            <div className=
+            "label-wrapper">
+              <label htmlFor='input-user-email'>
+                BE FIRST TO KNOW ABOUT RELEASES AND RESTOCKS
+              </label>
+            </div>
+            <div className='footer-email-input'>
+              <input type='text' placeholder='Email address' id ="input-user-email" />
+              <button type='button'>
+                SIGN UP 
+              </button>
+            </div>
+          </div>
+        </section>
+        <section className="footer-bottom-content"></section>
+      </Container>
+    </footer>
+  );
+}
+const mapDispatchToProps = (dispacth) => ({ dispacth });
+export default connect(null, mapDispatchToProps)(Footer);
