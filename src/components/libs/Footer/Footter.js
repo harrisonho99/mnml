35ca8971ac0ddd50c8a16d5885f2043de260054a
hotsnow199 <49254639+hotsnow199@users.noreply.github.com>
2021-03-16 13:44:51 +0700

    add footer

--- src/components/libs/Footer/Footter.js
+++ src/components/libs/Footer/Footter.js
@@ -1,21 +1,21 @@
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { connect } from "react-redux";
-import smallLogo from "../../../img/small-logo.png";
-import "./libstyle/footer.css";
-import { Container } from "@material-ui/core";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { connect } from 'react-redux';
+import smallLogo from '../../../img/small-logo.png';
+import './libstyle/footer.css';
+import { Container } from '@material-ui/core';
 function Footer({ dispacth }) {
-  const matchesMedia = useMediaQuery("(min-width:1000px)");
+  const matchesMedia = useMediaQuery('(min-width:1000px)');
 
   return (
     <footer>
-      {matchesMedia ? null : 1}
+      {/* {matchesMedia ? null : 1} */}
       <Container>
-        <section className={"footer-top-content"}>
-          <div className="footer-top-left">
-            <div className="footer-logo">
-              <img src={smallLogo} />
+        <section className={'footer-top-content'}>
+          <div className='footer-top-left'>
+            <div className='footer-logo'>
+              <img src={smallLogo} alt='logo' />
             </div>
-            <div className="footer-info">
+            <div className='footer-info'>
               <p>Search</p>
               <p>Contact Us</p>
               <p>Shipping</p>
@@ -25,22 +25,23 @@ function Footer({ dispacth }) {
               <p>Our story</p>
             </div>
           </div>
-          <div className="footer-top-right">
-            <div className=
-            "label-wrapper">
+          <div className='footer-top-right'>
+            <div className='label-wrapper'>
               <label htmlFor='input-user-email'>
                 BE FIRST TO KNOW ABOUT RELEASES AND RESTOCKS
               </label>
             </div>
             <div className='footer-email-input'>
-              <input type='text' placeholder='Email address' id ="input-user-email" />
-              <button type='button'>
-                SIGN UP 
-              </button>
+              <input
+                type='text'
+                placeholder='Email address'
+                id='input-user-email'
+              />
+              <button type='button'>SIGN UP</button>
             </div>
           </div>
         </section>
-        <section className="footer-bottom-content"></section>
+        <section className='footer-bottom-content'></section>
       </Container>
     </footer>
   );
