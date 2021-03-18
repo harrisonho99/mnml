819962e7bbd0827e4b54ab9c4a4ed228751c8454
hotsnow199 <49254639+hotsnow199@users.noreply.github.com>
2021-03-18 18:57:17 +0700

    filter mobile

--- src/components/libs/utils/Filter.js
+++ src/components/libs/utils/Filter.js
@@ -1,12 +1,29 @@
 import { useMediaQuery } from '@material-ui/core';
 import './utilStyle/Filter.css';
-import { useState } from 'react';
+import BlackButton from './BlackButton';
+import { useState, useRef, useEffect } from 'react';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 const Filter = () => {
+  const mobileFilterRef = useRef(null);
   const matchesMedia = useMediaQuery('(min-width:1000px)');
-  const [isMobileFilterExpand, setMobileFilterExpand] = useState(false);
+  // const [isMobileFilterExpand, setMobileFilterExpand] = useState(false);
   const hanldeMobileFilterExpand = () => {
-    setMobileFilterExpand(!isMobileFilterExpand);
+    if (mobileFilterRef.current) {
+      const filter = mobileFilterRef.current;
+      const classArr = Array.from(filter.classList);
+      if (classArr.length === 0) {
+        return filter.classList.add('filter-expand');
+      }
+      classArr.forEach((className) => {
+        if (className === 'filter-close') {
+          filter.classList.remove('filter-close');
+          filter.classList.add('filter-expand');
+        } else {
+          filter.classList.remove('filter-expand');
+          filter.classList.add('filter-close');
+        }
+      });
+    }
   };
   return (
     <>
@@ -21,6 +38,37 @@ const Filter = () => {
                 <KeyboardArrowDownIcon style={{ height: '100%' }} />
               </button>
             </h3>
+            <div ref={mobileFilterRef} id='mobile-filter' className=''>
+              <div id='mobile-filter-list'>
+                <div className='mobile-color-filter filter-items'>
+                  <h4>
+                    <span>COLOR </span>
+                    <KeyboardArrowDownIcon style={{ height: '100%' }} />
+                  </h4>
+                </div>
+                <div className='mobile-color-filter filter-items'>
+                  <h4>
+                    <span>PRICE </span>
+                    <KeyboardArrowDownIcon style={{ height: '100%' }} />
+                  </h4>
+                </div>
+                <div className='mobile-color-filter filter-items'>
+                  <h4>
+                    <span>SIZE </span>
+                    <KeyboardArrowDownIcon style={{ height: '100%' }} />
+                  </h4>
+                </div>
+                <div className='mobile-color-filter filter-items'>
+                  <h4>
+                    <span>TYPE </span>
+                    <KeyboardArrowDownIcon style={{ height: '100%' }} />
+                  </h4>
+                </div>
+              </div>
+              <div className='apply-filter-wrapper'>
+                <BlackButton type='button'>APPLY</BlackButton>
+              </div>
+            </div>
           </div>
         </>
       )}
