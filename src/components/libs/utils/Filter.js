dacf3557f4267c3a192045969e60bba8fd045699
hotsnow199 <49254639+hotsnow199@users.noreply.github.com>
2021-03-20 00:58:23 +0700

    add filter cart

--- src/components/libs/utils/Filter.js
+++ src/components/libs/utils/Filter.js
@@ -7,50 +7,66 @@ import { connect } from 'react-redux';
 import { filterAction } from '../../../redux/actionCreators/filterAction';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import { FILTER } from '../../../helper/_listNavURL';
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+
 const Filter = ({ dispatch, route }) => {
-  const mobileFilterRef = useRef(null);
   const [filtered, setfilterrd] = useState({
     filteredColor: null,
     filteredPrice: [FILTER.price.from, FILTER.price.to],
     filteredSize: null,
     filteredType: null,
   });
+  const mobileFilterRef = useRef(null);
+  const desktopFilterRef = useRef(null);
   const listFilterRef = useRef(null);
   const rangePriceRef = useRef(null);
   const matchesMedia = useMediaQuery('(min-width:1000px)');
-  // const [filterRangePrice, setFilterRangePrice] = useState([
-  //   FILTER.price.from,
-  //   FILTER.price.to,
-  // ]);
+
   //handle tongle filter mobile
   useEffect(() => {
+    //Mobile
+    //tongle filter forEach items
+    const toogleFilterItems = function () {
+      if (
+        !this.classList.contains('filter-item-expand') &&
+        !this.classList.contains('filter-item-close')
+      ) {
+        this.classList.add('filter-item-expand');
+      } else if (this.classList.contains('filter-item-close')) {
+        this.classList.remove('filter-item-close');
+        this.classList.add('filter-item-expand');
+      } else {
+        this.classList.remove('filter-item-expand');
+        this.classList.add('filter-item-close');
+      }
+    };
+    //Desk
+    //tongle filter forEach items
     if (listFilterRef.current) {
-      //tongle filter forEach items
-      const toogleFilterItems = function () {
-        if (
-          !this.classList.contains('filter-item-expand') &&
-          !this.classList.contains('filter-item-close')
-        ) {
-          this.classList.add('filter-item-expand');
-        } else if (this.classList.contains('filter-item-close')) {
-          this.classList.remove('filter-item-close');
-          this.classList.add('filter-item-expand');
-        } else {
-          this.classList.remove('filter-item-expand');
-          this.classList.add('filter-item-close');
-        }
-      };
-      const filterItems = Array.from(listFilterRef.current.childNodes);
-      filterItems.forEach((filter) => {
+      const mobileFilterItems = Array.from(listFilterRef.current.childNodes);
+      mobileFilterItems.forEach((filter) => {
         filter.addEventListener('click', toogleFilterItems);
       });
       return () => {
-        filterItems.forEach((filter) => {
+        mobileFilterItems.forEach((filter) => {
+          filter.removeEventListener('click', toogleFilterItems);
+        });
+      };
+    } else if (desktopFilterRef.current) {
+      const deskFilterItems = Array.from(desktopFilterRef.current.childNodes);
+      deskFilterItems.forEach((filter) => {
+        filter.addEventListener('click', toogleFilterItems);
+      });
+      return () => {
+        deskFilterItems.forEach((filter) => {
           filter.removeEventListener('click', toogleFilterItems);
         });
       };
     }
   }, [matchesMedia]);
+
   // handle move range propagate out side
   useEffect(() => {
     if (rangePriceRef.current) {
@@ -107,7 +123,117 @@ const Filter = ({ dispatch, route }) => {
   return (
     <>
       {matchesMedia ? (
-        <>DESKTOP</>
+        <>
+          <div id='desk-filter-wrapper'>
+            <div id='desk-filter-title' className='box-border'>
+              <Title level={4}>FILTERS</Title>
+            </div>
+            <div id='expandable-filter' ref={desktopFilterRef}>
+              <div id='desk-color-filter' className='box-border'>
+                <h4 className='desk-filter-title'>
+                  <span>Colors</span>
+                  <KeyboardArrowDownIcon style={{ height: '100%' }} />
+                </h4>
+                <div id='desk-color-picker' className='desk-mini-box'>
+                  {FILTER.color.map((color) => {
+                    return (
+                      <div
+                        key={color.value}
+                        className='color-item mini-box'
+                        style={{
+                          backgroundColor: color.value,
+                          border: `${
+                            filtered.filteredColor === color.value
+                              ? '5px solid #2979ff'
+                              : 'none'
+                          }`,
+                        }}
+                        onClick={() => {
+                          handleFilterColor(color.value);
+                        }}
+                      ></div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div id='desk-price-filter' className='box-border'>
+                <h4 className='desk-filter-title'>
+                  <span>Price Range</span>
+                  <KeyboardArrowDownIcon style={{ height: '100%' }} />
+                </h4>
+                <div className='desk-mini-box' id='desk-price-range-wrapper'>
+                  <Slider
+                    range
+                    defaultValue={filtered.filteredPrice}
+                    value={filtered.filteredPrice}
+                    onChange={handleRangeChange}
+                  />
+                  <p>
+                    From&nbsp;: {filtered.filteredPrice[0]}$
+                    &emsp;&emsp;&emsp;&emsp; To:&nbsp;
+                    {filtered.filteredPrice[1]}$
+                  </p>
+                </div>
+              </div>
+              <div id='desk-size-filter' className='box-border'>
+                <h4 className='desk-filter-title'>
+                  <span>Size</span>
+                  <KeyboardArrowDownIcon style={{ height: '100%' }} />
+                </h4>
+                <div className='desk-mini-box'>
+                  {FILTER.size.map((size) => {
+                    return (
+                      <div
+                        className='mini-box'
+                        key={size.value}
+                        onClick={() => {
+                          handleFilterSize(size.value);
+                        }}
+                        style={{
+                          border: `${
+                            filtered.filteredSize === size.value
+                              ? '3px solid #2979ff'
+                              : 'none'
+                          }`,
+                        }}
+                      >
+                        {size.value}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div id='desk-type-filter' className='box-border'>
+                <h4 className='desk-filter-title'>
+                  <span>Type</span>
+                  <KeyboardArrowDownIcon style={{ height: '100%' }} />
+                </h4>
+                <div className='desk-mini-box'>
+                  {FILTER.size.map((size) => {
+                    return (
+                      <div
+                        className='mini-box'
+                        key={size.value}
+                        onClick={() => {
+                          handleFilterSize(size.value);
+                        }}
+                        style={{
+                          border: `${
+                            filtered.filteredSize === size.value
+                              ? '3px solid #2979ff'
+                              : 'none'
+                          }`,
+                        }}
+                      >
+                        {size.value}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
       ) : (
         <>
           <div id='filter-box' className='filter-box-mobile'>
--- src/components/pages/ShopPage.js
+++ src/components/pages/ShopPage.js
@@ -1,13 +1,62 @@
 import './pagestyle/ShopPagee.css';
 import HomeMenu from '../libs/utils/HomeMenu';
 import Filter from '../libs/utils/Filter';
+import {
+  Container,
+  Grid,
+  Paper,
+  makeStyles,
+  useMediaQuery,
+} from '@material-ui/core';
+import SimpleCart from '../libs/utils/SimpleCart';
 
+const useStyles = makeStyles((theme) => {
+  return {
+    container: {
+      position: 'relative',
+      padding: theme.spacing(1),
+      margin: 0,
+    },
+    gridRoot: {},
+    paper: {
+      // margin: '10px 0',
+      background: 'white',
+      // backgroundColor: 'grey',
+      minHeight: '50vh',
+    },
+    filterContainer: {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+    },
+  };
+});
 const ShopPage = () => {
+  const classes = useStyles();
+  const matchesMedia = useMediaQuery('(min-width:1000px)');
   return (
     <>
-      <HomeMenu />
-      <Filter />
-      <main></main>
+      <main>
+        <Container className={classes.container}>
+          <HomeMenu />
+          <Grid container className={classes.gridRoot}>
+            <Grid item xs={12} md={3}>
+              <Filter />
+            </Grid>
+            <Grid item xs={12} md={9}>
+              <Paper elevation={0} className={classes.paper}>
+                <SimpleCart
+                  color='white'
+                  imgURL='https://cdn.shopify.com/s/files/1/1300/6871/products/vintage-cargo-pants-black-2_160x.jpg?v=1564305047%20160w,%20//cdn.shopify.com/s/files/1/1300/6871/products/vintage-cargo-pants-black-2_320x.jpg?v=1564305047%20320w,%20//cdn.shopify.com/s/files/1/1300/6871/products/vintage-cargo-pants-black-2_650x.jpg?v=1564305047%20650w,%20//cdn.shopify.com/s/files/1/1300/6871/products/vintage-cargo-pants-black-2_1300x.jpg?v=1564305047%201300w'
+                  price={50}
+                  name='SIMPLE DENIM'
+                  prodURL='1003131'
+                />
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
     </>
   );
 };
